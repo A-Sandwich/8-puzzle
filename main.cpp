96e435f916f8bba8a81c603a31723ad94f46c50d
A-Sandwich <kbburkholder@gmail.com>
2014-02-27 11:20:28 -0500

    Added some base functionality. Still doesn't work though. Did some debugging and code is more sound though.

--- main.cpp
+++ main.cpp
@@ -124,7 +124,8 @@ Node* Create_Node(int **passed_board){
     Node *temp;
     temp = new Node;
     temp->board = passed_board;//set node's
-    temp->next = NULL;
+    temp->next = 0;
+    //cout << "Node Created" << endl;
     return temp;
 }
 
@@ -142,38 +143,59 @@ int** Swap_Tile(int **board, int row, int column, int dx, int dy){
         }
     }
 
-    //make switch
-    cout << "In Function: " << row << ", " << column << endl;
-    cout << dx << ", " << dy << endl;
     temp = temp_board[row][column];
     temp_board[row][column] = temp_board[row + dx][column + dy];
     temp_board[row + dx][column + dy] = temp;
-
+    //cout << "Leaving Swap Tile " << endl;
     return temp_board;
 }
 
 bool In_Closed_List(Node *head, int **board){
-    bool equivalent = true;
+    //cout << "In closed list" << endl;
+    bool in_list = true;
     Node *temp;
     temp = head;
-    if(head == NULL)
+
+    if(head == NULL){
         return false;
-    if(temp != NULL){
-        while(temp->next != NULL){
-            for(int i=0; i<ROW_SIZE;i++){
-                for(int j=0;j<COLUMN_SIZE;j++){
-                    if(board[i][j] != temp->board[i][j])
-                        equivalent = false;
-                }
+    }else if(head->next == NULL || head->next == 0){
+        for(int i=0;i<ROW_SIZE;i++){
+            for(int j=0;j<COLUMN_SIZE;j++){
+                if(head->board[i][j] != board[i][j])
+                    in_list = false;
             }
-            if(equivalent == true)
-                return equivalent;
-            temp = temp->next;
         }
+        return in_list;
     }
 
-    return equivalent;
 
+    while(temp->next != 0){
+        temp = temp->next;
+        in_list = true;
+        for(int i=0;i<ROW_SIZE;i++){
+            for(int j=0;j<COLUMN_SIZE;j++){
+                if(temp->board[i][j] != board[i][j])
+                    in_list = false;
+            }
+        }
+
+        if(in_list)
+            return in_list;
+    }
+
+    return in_list;
+}
+
+void Add_To_Closed_List(Node *new_node, Node *head){
+    Node *temp;
+
+    temp = head;
+
+    //traverse to end of list
+    while(temp->next != NULL && temp->next != 0){
+        temp = temp->next;
+    }
+    temp->next = new_node;
 }
 
 //function that loops through the three boards looking for a solution.
@@ -183,89 +205,119 @@ bool In_Closed_List(Node *head, int **board){
 void Breadth_First_Search(int **board1, int **board2, int **board3){
     bool keepgoing = true, board1_solved = false, board2_solved = false, board3_solved = false;
     int row;
-    int column, temp;
+    int column, temp, fringe_size = 1, closed_size = 1;
     int **temp_board;
+    int counter = 0;
     Node *tail1, *tail2, *tail3, *fringe1, *fringe2, *fringe3, *closed1, *closed2, *closed3, *null, *temp_node;
+
     fringe1 = Create_Node(board1);
     fringe2 = Create_Node(board2);
     fringe3 = Create_Node(board3);
 
     null = NULL;
-    closed1 = NULL;
-    closed2 = NULL;
-    closed3 = NULL;
+    closed1 = Create_Node(board1);
+    closed2 = Create_Node(board2);
+    closed3 = Create_Node(board3);
 
     tail1 = fringe1;
     tail2 = fringe2;
     tail3 = fringe3;
 
     while(keepgoing){
+        counter++;
+        if(counter%1000 == 0){
+            cout << "While loop itterations: " << counter << endl;
+            cout << "Fringe size: " << fringe_size << endl;
+            cout << "Closed list size: " << closed_size << endl;
+        }
+        //cout << "here" << endl;
+        //cout << board1_solved << endl;
+        //Print_Board(fringe1->board);
         //check for solutions:
-        if((board1_solved&&board2_solved) || (board2_solved&&board3_solved) || (board1_solved&&board3_solved))
+        if((board1_solved&&board2_solved) || (board2_solved&&board3_solved) || (board1_solved&&board3_solved) || board1_solved)
             keepgoing = false;
+
         Find_Blank(row, column, fringe1->board);
+        //cout << row << ", " << column << endl;
         if(!board1_solved && row - 1 >=0){
             temp_board = Swap_Tile(fringe1->board, row, column, -1, 0);
+            //cout << In_Closed_List(closed1, temp_board) << endl;
             if(In_Closed_List(closed1, temp_board) == false){
                 if(Solution_Found(temp_board)){
                     board1_solved = true;
-                }else{
-
+                    Print_Board(temp_board);
                 }
-
                 tail1->next = Create_Node(temp_board);
                 tail1 = tail1->next;
+                Add_To_Closed_List(Create_Node(tail1->board), closed1);
+                fringe_size++;
+                closed_size++;
             }//end if
         }
 
         if(!board1_solved && row + 1 <=2){
             temp_board = Swap_Tile(fringe1->board, row, column, 1, 0);
+            //cout << In_Closed_List(closed1, temp_board) << endl;
             if(In_Closed_List(closed1, temp_board) == false){
                 if(Solution_Found(temp_board)){
                     board1_solved = true;
-                }else{
-
+                    Print_Board(temp_board);
                 }
                 tail1->next = Create_Node(temp_board);
                 tail1 = tail1->next;
+                Add_To_Closed_List(Create_Node(tail1->board), closed1);
+                fringe_size++;
+                closed_size++;
             }//end if
         }
 
         if(!board1_solved && column - 1 >=0){
             temp_board = Swap_Tile(fringe1->board, row, column, 0, -1);
+            //cout << In_Closed_List(closed1, temp_board) << endl;
             if(In_Closed_List(closed1, temp_board) == false){
                 if(Solution_Found(temp_board)){
                     board1_solved = true;
-                }else{
-
+                    Print_Board(temp_board);
                 }
                 tail1->next = Create_Node(temp_board);
                 tail1 = tail1->next;
+                Add_To_Closed_List(Create_Node(tail1->board), closed1);
+                fringe_size++;
+                closed_size++;
             }//end if
         }
 
         if(!board1_solved && column + 1 <=2){
             temp_board = Swap_Tile(fringe1->board, row, column, 0, 1);
+            //cout << In_Closed_List(closed1, temp_board) << endl;
             if(In_Closed_List(closed1, temp_board) == false){
                 if(Solution_Found(temp_board)){
                     board1_solved = true;
-                }else{
-
+                    Print_Board(temp_board);
                 }
                 tail1->next = Create_Node(temp_board);
                 tail1 = tail1->next;
+                Add_To_Closed_List(Create_Node(tail1->board), closed1);
+                fringe_size++;
+                closed_size++;
             }//end if
         }
+
         if(!board1_solved){
+            fringe_size--;
+            //cin.ignore();
             temp_node = fringe1;
+            //cout << fringe1->next;
             fringe1 = fringe1->next;
+            //cout << fringe1->next;
             delete temp_node;
+            //cin.ignore();
         }
 
-        //cout << row << column << endl;
-        //Print_Board(Swap_Tile(fringe1->board, row, column, -1, 0));
-        //Print_Board(fringe1->board);
-        //keepgoing = false;
+
+
 
     }//end while
+
+
 }//end breadth first search
